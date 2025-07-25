37740454271e10f399e4036344e7a8e04727975c
ZLATAN628 <107628611+ZLATAN628@users.noreply.github.com>
2025-07-25 10:08:04 +0800

    linux 和 windows 模块补充

--- src/clipboard/mod.rs
+++ src/clipboard/mod.rs
@@ -1,5 +1,15 @@
 mod common;
 
+#[cfg(windows)]
+mod win32;
+#[cfg(windows)]
+pub use win32::{Master, Shutdown};
+
+#[cfg(all(unix, not(any(target_os="macos", target_os="ios", target_os="android", target_os="emscripten"))))]
+mod x11;
+#[cfg(all(unix, not(any(target_os="macos", target_os="ios", target_os="android", target_os="emscripten"))))]
+pub use x11::{Master, Shutdown};
+
 #[cfg(target_os = "macos")]
 mod mac;
 #[cfg(target_os = "macos")]
